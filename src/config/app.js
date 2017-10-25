3cf9213591410dfe255e4c4a7c2c9e5f0a90dc00
Megan Koh <dearmegan@outlook.com>
2017-10-25 01:23:33 -0400

    styling and file structure changes

--- src/config/app.js
+++ src/config/app.js
@@ -1,13 +1,14 @@
 import React, { Component } from 'react';
 import Menu from '../containers/menu';
 import Routes from './routes';
+import "./index.css";
 
 class App extends Component {
 
     render() {
         return (
             <div>
-                <Menu />
+                <div className="menu-fixed"><Menu /></div>
                 <div className="main"><Routes /></div>
             </div>
         );
--- webpack.config.js
+++ webpack.config.js
@@ -1,28 +1,35 @@
 const path = require('path');
+const webpack = require("webpack");
 
 module.exports = {
-  context: path.join(__dirname, 'src'),
-  entry: [
-    './index.js',
-  ],
-  output: {
-    path: path.join(__dirname, 'www'),
-    filename: 'bundle.js',
-  },
-  module: {
-    rules: [
-      {
-        test: /\.js$/,
-        exclude: /node_modules/,
-        use: [
-          'babel-loader',
+    context: path.join(__dirname, 'src'),
+    entry: [ './index.js', ],
+
+    output: {
+        path: path.join(__dirname, 'www'),
+        filename: 'bundle.js',
+    },
+
+    module: {
+        rules: [
+            {
+                test: /\.js$/,
+                exclude: /node_modules/,
+                use: [ 'babel-loader' ],
+            },
+            {
+                test: /.css$/,
+                use: [ 'style-loader', 'css-loader' ]
+            },
+            {
+                test: /\.(png|woff|woff2|eot|ttf|svg)$/, 
+                loader: 'url-loader?limit=100000' }
+        ]
+    },
+
+    resolve: {
+        modules: [
+            path.join(__dirname, 'node_modules'),
         ],
-      },
-    ],
-  },
-  resolve: {
-    modules: [
-      path.join(__dirname, 'node_modules'),
-    ],
-  },
+    },
 };
